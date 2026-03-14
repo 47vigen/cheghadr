ca3702cab6c3771614434ad16bfaa507426990f6
Vigen <47vigen@gmail.com>
2026-03-15 02:49:47 +0330

    refactor: update ESLint and package configurations
    - Standardize import statements in eslint.config.mjs and postcss.config.mjs to single quotes.
    - Modify package.json scripts to include additional commands for type checking, formatting, and linting with biome.
    - Update dependencies in package.json and pnpm-lock.yaml, replacing eslint with biome and adding commitlint packages.
    - Adjust layout and button components for consistent styling and improved readability.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,19 @@
-import { Geist, Geist_Mono } from "next/font/google"
+import { Geist, Geist_Mono } from 'next/font/google'
 
-import "./globals.css"
-import { ThemeProvider } from "@/components/theme-provider"
-import { cn } from "@/lib/utils";
+import './globals.css'
+
+import { ThemeProvider } from '@/components/theme-provider'
+
+import { cn } from '@/lib/utils'
 
 const fontSans = Geist({
-  subsets: ["latin"],
-  variable: "--font-sans",
+  subsets: ['latin'],
+  variable: '--font-sans',
 })
 
 const fontMono = Geist_Mono({
-  subsets: ["latin"],
-  variable: "--font-mono",
+  subsets: ['latin'],
+  variable: '--font-mono',
 })
 
 export default function RootLayout({
@@ -23,7 +25,12 @@ export default function RootLayout({
     <html
       lang="en"
       suppressHydrationWarning
-      className={cn("antialiased", fontMono.variable, "font-sans", fontSans.variable)}
+      className={cn(
+        'antialiased',
+        fontMono.variable,
+        'font-sans',
+        fontSans.variable,
+      )}
     >
       <body>
         <ThemeProvider>{children}</ThemeProvider>
